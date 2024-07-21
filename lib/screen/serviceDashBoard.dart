dba39a5606cd7dd4e9a3a669421ea220e2cf85ca
lahiruileperumage <lahiru212001@gmail.com>
2024-07-21 17:04:42 +0530

    discount section create

--- lib/screen/serviceDashBoard.dart
+++ lib/screen/serviceDashBoard.dart
@@ -29,47 +29,60 @@ class _ServicedashboardState extends State<Servicedashboard> {
                             width: 50,
                             height: 50,
                             child: Image.network(
-                                'https://www.nicepng.com/png/full/182-1829287_cammy-lin-ux-designer-circle-picture-profile-girl.png'),
+                              'https://www.nicepng.com/png/full/182-1829287_cammy-lin-ux-designer-circle-picture-profile-girl.png',
+                            ),
                           ),
                         ],
                       ),
                     ),
                     const Spacer(),
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
                       child: Column(
                         children: [
-                          Text("Hi, Anna ✋",
-                              style: TextStyle(color: Colors.black)),
+                          Text(
+                            "Hi, Anna ✋",
+                            style: TextStyle(color: Colors.black),
+                          ),
                           Row(
                             children: [
-                              Icon(FontAwesomeIcons.locationDot,
-                                  color: Colors.blue),
+                              Icon(
+                                FontAwesomeIcons.locationDot,
+                                color: Colors.blue,
+                              ),
                               SizedBox(width: 4), // Space between icon and text
-                              Text("Panadura, Sri Lanka",
-                                  style: TextStyle(color: Colors.grey)),
+                              Text(
+                                "Panadura, Sri Lanka",
+                                style: TextStyle(color: Colors.grey),
+                              ),
                             ],
                           ),
                         ],
                       ),
                     ),
-                    Spacer(),
+                    const Spacer(),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
                         children: [
-                          Container(
-                            width: 40,
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(color: Colors.grey),
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Center(
-                              child: FaIcon(
-                                FontAwesomeIcons.bell,
-                                color: Colors.black,
+                          InkWell(
+                            onTap: () {
+                              // Handle the button press
+                              print('Notification icon pressed');
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(color: Colors.grey),
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: FaIcon(
+                                  FontAwesomeIcons.bell,
+                                  color: Colors.black,
+                                ),
                               ),
                             ),
                           ),
@@ -86,7 +99,7 @@ class _ServicedashboardState extends State<Servicedashboard> {
                         child: TextField(
                           decoration: InputDecoration(
                             hintText: 'How Can help you today?',
-                            prefixIcon: Icon(Icons.search),
+                            prefixIcon: const Icon(Icons.search),
                             fillColor: Colors.grey[200],
                             filled: true,
                             border: const OutlineInputBorder(
@@ -95,13 +108,133 @@ class _ServicedashboardState extends State<Servicedashboard> {
                                   BorderRadius.all(Radius.circular(12.0)),
                             ),
                             contentPadding: const EdgeInsets.symmetric(
-                                vertical: 0.0, horizontal: 16.0),
+                              vertical: 0.0,
+                              horizontal: 16.0,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ],
                 ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Today Special",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "See All",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue, // Background color of the container
+                    borderRadius: BorderRadius.circular(
+                        10.0), // Border radius of the container
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "25% OFF",
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "Get discount for every order,",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "only valid for today",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          8.0), // Add spacing between text and button
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      // Add your button action here
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.blue,
+                                      backgroundColor: Colors
+                                          .white, // Text color of the button
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            20.0), // Border radius of the button
+                                      ),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 16.0,
+                                          vertical:
+                                              8.0), // Padding inside the button
+                                    ),
+                                    child: Text(
+                                      "Get Discount",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.network(
+                              'https://i.ibb.co/61rxQL1/Daco-4431776.png',
+                              width: 100, // Adjust width as needed
+                              height: 150, // Adjust height as needed
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
