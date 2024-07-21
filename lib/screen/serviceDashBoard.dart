5b45298b940d8d497f63131ee2f89399202c7d0f
lahiruileperumage <lahiru212001@gmail.com>
2024-07-21 14:55:39 +0530

    create header and search bar

--- lib/screen/serviceDashBoard.dart
+++ lib/screen/serviceDashBoard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Servicedashboard extends StatefulWidget {
   const Servicedashboard({super.key});
@@ -13,48 +14,98 @@ class _ServicedashboardState extends State<Servicedashboard> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              width: 50,
-                              height: 50,
-                              child: Image.network(
-                                  'https://www.nicepng.com/png/full/182-1829287_cammy-lin-ux-designer-circle-picture-profile-girl.png'),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Image.network(
+                                'https://www.nicepng.com/png/full/182-1829287_cammy-lin-ux-designer-circle-picture-profile-girl.png'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text("Hi, Anna ✋",
+                              style: TextStyle(color: Colors.black)),
+                          Row(
+                            children: [
+                              Icon(FontAwesomeIcons.locationDot,
+                                  color: Colors.blue),
+                              SizedBox(width: 4), // Space between icon and text
+                              Text("Panadura, Sri Lanka",
+                                  style: TextStyle(color: Colors.grey)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(color: Colors.grey),
+                              shape: BoxShape.circle,
                             ),
-                          ],
+                            child: const Center(
+                              child: FaIcon(
+                                FontAwesomeIcons.bell,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'How Can help you today?',
+                            prefixIcon: Icon(Icons.search),
+                            fillColor: Colors.grey[200],
+                            filled: true,
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12.0)),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 0.0, horizontal: 16.0),
+                          ),
                         ),
                       ),
-                      Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text("Hi, name ✋"),
-                            Text("📍location, Sri Lanka")
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [Icon(Icons.notifications_active)],
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            )),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
